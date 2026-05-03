17c0ca4a7fa98a060186a05363d06e781efe39fe
andrew eaker <andewaker@gmail.com>
2026-05-02 20:56:04 -0400

    fix: actually fixed student tests

--- packages/smooth_app/test/student_tests/test_suite_2/future_date_inaccessable_test.dart
+++ packages/smooth_app/test/student_tests/test_suite_2/future_date_inaccessable_test.dart
@@ -62,8 +62,8 @@ void main() {
         colorProvider,
         ChangeNotifierProvider<PriceModel>.value(
           value: model,
-          child: const Scaffold(
-            body: ListView(children: <Widget>[PriceDateCard()]),
+          child: Scaffold(
+            body: ListView(children: const <Widget>[PriceDateCard()]),
           ),
         ),
       ),
